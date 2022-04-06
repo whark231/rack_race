16097e2982dc2aad2c81534e81a1e280b7d88044
Patrick Kehinde Jr <pkehinde@seas.upenn.edu>
2022-04-06 18:24:48 -0400

    ADDED A RESET PASSWORD BUTTON

--- client/src/Auth/Login.js
+++ client/src/Auth/Login.js
@@ -1,11 +1,59 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useRef } from 'react'
 import { Button, TextField, Stack } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 import useAuth from '../hooks/useAuth'
 
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [needsReset,setNeedsReset] = useState(false);
+  const answer = useRef("");
+  const navigate = useNavigate();
+  
+
   const { login, error } = useAuth();
+
+
+  //NOT FINISHED WITH RESET PASWORD 
+
+  function verify(username,answer){
+    setNeedsReset(false);
+    navigate("/users");
+    return true;
+  }
+
+
+
+  if(needsReset){
+
+    return (
+      <div className='col'>
+        <h1>Reset Password</h1>
+        <Stack spacing={3}>
+          {error &&
+            <div style={{color: "red"}}>{error}</div>
+          }
+          <TextField
+              label="username"
+              onChange={(e)=>setUsername(e.target.value)}
+              size="small"
+            />
+          <TextField
+            label="Enter maiden name of mother"
+            onChange={(e)=> answer.current = e.target.value}
+            type={"password"}
+            size="small"
+          />
+          {/* Submit */}
+          <Button 
+            variant="contained" 
+            onClick={() => verify(username, answer.current)}
+          >Verify</Button>
+        </Stack>
+      </div>
+    )
+
+  }
 
   return (
     <div className='col'>
@@ -30,7 +78,17 @@ export default function Login() {
           variant="contained" 
           onClick={() => login(username, password)}
         >Login</Button>
+        <Button 
+          variant="contained" 
+          onClick={() => setNeedsReset(true)}
+        >Reset Password</Button>
+
       </Stack>
     </div>
   )
+
+
+
+
+
 }
