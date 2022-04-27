86167387caf54a3a40b4f3b59b7aa7a6b150c8c9
wharkpenn <97925439+wharkpenn@users.noreply.github.com>
2022-04-27 18:23:42 -0400

    updated tests

--- client/cypress/integration/auth_ui.spec.js
+++ client/cypress/integration/auth_ui.spec.js
@@ -10,13 +10,13 @@ describe("test auth screens", () => {
   it("goes to login page", () => {
     cy.visit("http://localhost:3000/");
     cy.get(".clickable").contains("login").click();
-    cy.url().should("http://localhost:3000/login");
+    cy.url().should("eq", "http://localhost:3000/login");
   });
 
   it("goes to register page", () => {
     cy.visit("http://localhost:3000/");
     cy.get(".clickable").contains("register").click();
-    cy.url().should("http://localhost:3000/register");
+    cy.url().should("eq", "http://localhost:3000/register");
   });
 
   it("creates a new user", () => {
@@ -27,7 +27,7 @@ describe("test auth screens", () => {
     cy.getbyLabelText("password").type("Cypress").should("have.value", "Cypress");
     cy.getbyLabelText("charity").type("Cypress").should("have.value", "Cypress");
     cy.get("Button").contains("Submit");
-    cy.url().should("http://localhost:3000/login");
+    cy.url().should("eq", "http://localhost:3000/login");
   });
 
   it("logs into the app", () => {
@@ -35,7 +35,7 @@ describe("test auth screens", () => {
     cy.getbyLabelText("username").type("willhark");
     cy.getbyLabelText("password").type("test123");
     cy.get("Button").contains("Submit");
-    cy.url().should("http://localhost:3000/");
+    cy.url().should("eq", "http://localhost:3000/");
   });
 
   // it("submits the username and routes to", () => {
