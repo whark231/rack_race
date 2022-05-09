14c3dd18d1ffb262dc2ff949ef0d030b453c3968
Aisha Olapade <aishaola@seas.upenn.edu>
2022-05-09 10:12:17 -0400

    Added controller with create + find functions

---
+++ server/controllers/WorkoutController.js
@@ -0,0 +1,26 @@
+const WorkoutModel = require('../models/Workout');
+
+const WorkoutController = {
+    find: async (req, res) => {
+        const { id } = req.params;
+        try {
+            const data = await WorkoutModel.findById(id)
+            res.status(200).send(data);
+        } catch (err) {
+            res.status(400).send(err.message);
+            console.log(err);
+        }
+    }, 
+    create: async (req, res) => {
+		const { user, workout_length, location, description, date, time, } = req.body;
+		const workout = new WorkoutModel({ user: user, workout_length: workout_length, location: location, description: description, date: date, time: time, });
+        try {  
+            await workout.save();
+            res.status(200).send('data created!');
+            console.log('Workout created!');
+        } catch (err) {
+            res.status(500).send(err);
+            console.log(err);
+        }
+  },
+}
