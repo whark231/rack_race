a5fa0cbd359991c4552fc7a26b1da3ae452a9127
wharkpenn <97925439+wharkpenn@users.noreply.github.com>
2022-05-10 04:00:14 -0400

    updated tests

--- client/cypress/integration/user_ui.spec.js
+++ client/cypress/integration/user_ui.spec.js
@@ -29,7 +29,7 @@ describe("testing routing to different pages from landing page", () => {
     cy.url().should('eq', 'http://localhost:3000/settings');
   });
 
-  it("route to settings", () => {
+  it("logout", () => {
     cy.visit("http://localhost:3000/login");
     cy.get('#username').type("username").should("have.value", "username");
     cy.get('#password').type("password").should("have.value", "password");
